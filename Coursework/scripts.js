e91fa51e85257f8b40f6584f9db700681605957e
dstanger <dylanstanger@icloud.com>
2025-05-10 18:04:50 +0100

    Added some Js to the first page, performs db queries

---
+++ Coursework/scripts.js
@@ -0,0 +1,58 @@
+import { createClient } from 
+    'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabase = createClient('https://ktrhdcncdjcclfhwetga.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0cmhkY25jZGpjY2xmaHdldGdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwOTIwNjcsImV4cCI6MjA2MTY2ODA2N30.1D0hRo409xONRqaErQtd82pzFHl9ggI5J5qWOU7zo78')
+
+async function PeopleSearchFunc() {
+    
+    const name = document.getElementById('name').value.trim();
+    const license = document.getElementById('license').value.trim();
+    const results = document.getElementById('results');
+    const message = document.getElementById('message');
+    let data = null;
+    let error = null;
+
+
+    if((name&&license) || (!name&&!license) ) {
+        message.textContent = "Error: exactly one of the forms must be used";
+    } else {
+
+        if(name) {
+            const response = await supabase
+                .from('People')
+                .select('*')
+                .ilike('Name', `%${name}%`);
+            data = response.data;
+            error = response.error;
+        }
+        else if (license) {
+            const response2 = await supabase
+                .from('People')
+                .select('*')
+                .ilike('LicenseNumber', `%${license}%`);
+            data = response2.data;
+            error = response2.error;
+        }
+        
+      
+        if(data==0)
+            message.textContent = 'No result found'
+        else {
+            message.textContent = 'Search successful'                
+            results.textContent = JSON.stringify(data, null, 2);
+        }
+            
+
+    }
+}
+document.getElementById('driverName').addEventListener('submit', function (event) {
+    event.preventDefault();
+    PeopleSearchFunc(); 
+});
+document.getElementById('licenseNumber').addEventListener('submit', function (event) {
+    event.preventDefault();
+    PeopleSearchFunc(); 
+});
+document.getElementById('submit').addEventListener('click', PeopleSearchFunc);
+
+
